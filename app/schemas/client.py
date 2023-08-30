93f698f4e6473cb77c3a9d942177525f460d51aa
Eduardo Mendes <eduardompereira4@outlook.com>
2023-08-30 20:10:39 -0300

    feat: criação dos schemas client, session e realm

---
+++ app/schemas/client.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+
+class ClientBase(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=255)
+    key: str = Field(min_length=1, max_length=50)
+    secret: str = Field(min_length=1, max_length=255)
+    realm_id: int
+
+
+class ClientCreate(ClientBase):
+    pass
+
+
+class ClientUpdate(ClientBase):
+    pass
+
+
+class ClientShow(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
---
+++ app/schemas/session.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+
+class SessionBase(BaseModel):
+    token: str = Field(min_length=1, max_length=255)
+    is_active: bool = Field(default=True)
+    #user_id: int
+
+
+class SessionCreate(SessionBase):
+    pass
+
+
+class SessionUpdate(SessionBase):
+    pass
+
+
+class SessionShow(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
