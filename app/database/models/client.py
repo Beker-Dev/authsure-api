1c38ed21a4845e6eedcf09882463825dd2bab029
Eduardo Mendes <eduardompereira4@outlook.com>
2023-08-30 19:59:03 -0300

    fix: correções dos atributos das models

--- app/database/models/client.py
+++ app/database/models/client.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, ForeignKey, String, Boolean
+from sqlalchemy import Column, Integer, ForeignKey, String, Boolean, Text
 from sqlalchemy.orm import relationship
 
 from .base import Base
 
 class Client(Base):
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    description = Column(String(255), nullable=False)
+    name = Column(String(50), nullable=False)
+    description = Column(Text(255), nullable=True)
     key = Column(String(50), nullable=False)
-    secret_char = Column(String(255), nullable=False)
+    secret = Column(String(255), nullable=False)
     realm_id = Column(Integer, ForeignKey('realm.id'))
