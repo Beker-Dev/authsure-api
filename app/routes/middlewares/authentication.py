fa463ac3734e55fa105e233ccbc0acc7ba348867
Beker-Dev <joaopedro.beker@hotmail.com>
2023-11-02 19:00:24 -0300

    test: auth

--- app/routes/middlewares/authentication.py
+++ app/routes/middlewares/authentication.py
@@ -13,20 +13,22 @@ class Authentication:
         self.jwt = JWT()
 
     async def __call__(self, request: Request, call_next: Callable) -> JSONResponse:
-        bypass_routes = ['/docs', '/openapi.json', '/api/auth/login', '/api/auth/refresh']
+        return await call_next(request)
 
-        if request.url.path in bypass_routes:
-            return await call_next(request)
-
-        try:
-            token = request.headers.get('authorization').split('Bearer ')[1]
-            self.jwt.jwt_token_validator(token)
-            self.current_user.token = token
-
-            session = session_repository.get_by(next(get_db()), {'token': token})
-            if not session or not session.is_active:
-                raise Exception
-        except:
-            return JSONResponse(content='Invalid Token', status_code=401)
-        else:
-            return await call_next(request)
+        # bypass_routes = ['/docs', '/openapi.json', '/api/auth/login', '/api/auth/refresh']
+        #
+        # if request.url.path in bypass_routes:
+        #     return await call_next(request)
+        #
+        # try:
+        #     token = request.headers.get('Authorization').split('Bearer ')[1]
+        #     self.jwt.jwt_token_validator(token)
+        #     self.current_user.token = token
+        #
+        #     session = session_repository.get_by(next(get_db()), {'token': token})
+        #     if not session or not session.is_active:
+        #         raise Exception
+        # except Exception as e:
+        #     return JSONResponse(content={'detail': 'Invalid token'}, status_code=401)
+        # else:
+        #     return await call_next(request)
