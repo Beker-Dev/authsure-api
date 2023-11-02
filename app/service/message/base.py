db92d2c3c024160b6e91a6235e7c3125eead67b1
Eduardo <eduardompereira4@outlook.com>
2023-11-02 15:50:15 -0300

    update constructor message broker

--- app/service/message/base.py
+++ app/service/message/base.py
@@ -2,10 +2,11 @@ import asyncio
 from aio_pika import connect, Message
 
 class MessageBroker:
-    def __init__(self, connection_parameters):
+    def __init__(self, connection_parameters, name_queue):
         self.connection = None
         self.channel = None
         self.listener_queue = None
+        self.name_queue = name_queue
         self.exchange = None
         self.connection_parameters = connection_parameters
 
@@ -16,7 +17,7 @@ class MessageBroker:
         self.channel = await self.connection.channel()
 
     async def declare_queue_and_exchange(self):
-        self.listener_queue = await self.channel.declare_queue("my_queue")
+        self.listener_queue = await self.channel.declare_queue(self.name_queue)
         self.exchange = await self.channel.declare_exchange("my_exchange", type="direct")
         await self.listener_queue.bind(self.exchange, "my_routing_key")
 
@@ -29,19 +30,3 @@ class MessageBroker:
             async for message in queue_iter:
                 async with message.process():
                     print("Mensagem recebida:", message.body.decode())
-
-
-async def main():
-    connection_parameters = "amqp://localhost/"
-    message_broker = MessageBroker(connection_parameters)
-    await message_broker.connect_to_rabbitmq()
-    await message_broker.create_channel()
-    await message_broker.declare_queue_and_exchange()
-
-    await message_broker.publish_message("Hello, World!")
-
-    await message_broker.consume_messages()
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
