3ecfcba13376d4825a53f1d9eadbda9ad5a91ef6
Beker-Dev <joaopedro.beker@hotmail.com>
2023-12-01 19:31:45 -0300

    fix: association update tables

--- app/repository/group.py
+++ app/repository/group.py
@@ -4,6 +4,7 @@ from app.schemas.group import GroupCreate, GroupUpdate
 from app.utils.repository_utils.get_repository import get_repository_by_name
 
 from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
 
 
 class GroupRepository(RepositoryBase[Group, GroupCreate, GroupUpdate]):
@@ -15,6 +16,18 @@ class GroupRepository(RepositoryBase[Group, GroupCreate, GroupUpdate]):
         obj_in.users = users
         obj_in.roles = roles
         return super().create_with_association(db, obj_in)
+
+    def update(self, db: Session, db_obj: User, obj_in: GroupUpdate) -> Group:
+        user_repository = get_repository_by_name("user", "user_repository")
+        role_repository = get_repository_by_name("role", "role_repository")
+        users = [user_repository.get(db, user_id) for user_id in obj_in.users]
+        roles = [role_repository.get(db, role_id) for role_id in obj_in.roles]
+        obj_in.users = users
+        obj_in.roles = roles
+        custom_obj_data = jsonable_encoder(db_obj)
+        custom_obj_data["users"] = jsonable_encoder(db_obj.users)
+        custom_obj_data["roles"] = jsonable_encoder(db_obj.roles)
+        return super().update(db, db_obj, obj_in, custom_obj_data)
 
 
 group_repository = GroupRepository(Group)
--- app/repository/role.py
+++ app/repository/role.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+
 
 from .base import RepositoryBase
 from app.database.models.role import Role
@@ -12,6 +14,14 @@ class RoleRepository(RepositoryBase[Role, RoleCreate, RoleUpdate]):
         clients = [client_repository.get(db, client_id) for client_id in obj_in.clients]
         obj_in.clients = clients
         return super().create_with_association(db, obj_in)
+
+    def update(self, db: Session, db_obj: User, obj_in: RoleUpdate) -> Role:
+        client_repository = get_repository_by_name("client", "client_repository")
+        clients = [client_repository.get(db, client_id) for client_id in obj_in.clients]
+        obj_in.clients = clients
+        custom_obj_data = jsonable_encoder(db_obj)
+        custom_obj_data["clients"] = jsonable_encoder(db_obj.clients)
+        return super().update(db, db_obj, obj_in, custom_obj_data)
 
 
 role_repository = RoleRepository(Role)
